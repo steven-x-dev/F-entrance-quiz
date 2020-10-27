dfb9afd282ad7fa88f0fbdae19454ecdfc99e7c1
steven-x-dev <steven-dev@qq.com>
2020-10-27 22:32:18 +0800

    [steven] feat: added title bar for grouping functionality

--- src/App/Home.js
+++ src/App/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import StudentList from './components/StudentList';
+import Groups from './components/Groups';
 import './Home.scss';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
+        <Groups />
         <StudentList />
       </div>
     );
---
+++ src/App/components/Groups.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Groups.scss';
+
+class Groups extends Component {
+  render() {
+    return (
+      <div className="Groups">
+        <div className="title">
+          <h3 className="title-text">分组列表</h3>
+          <div className="btn group-btn">分组学员</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Groups;
