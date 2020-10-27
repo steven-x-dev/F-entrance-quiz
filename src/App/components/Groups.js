1215eef2caebcf6ea55817d61b01d54df827d40b
steven-x-dev <steven-dev@qq.com>
2020-10-27 22:39:06 +0800

    [steven] feat: changed cursor to pointer for active buttons

--- src/App/components/Groups.js
+++ src/App/components/Groups.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './Groups.scss';
 
 class Groups extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickable: true
+    };
+  }
+
   render() {
+    const { clickable } = this.state;
     return (
       <div className='Groups'>
         <div className='title'>
           <h3 className='title-text'>分组列表</h3>
-          <div className='btn group-btn'>分组学员</div>
+          <div className={`btn ${clickable ? 'btn-active' : ''} group-btn`}>分组学员</div>
         </div>
       </div>
     );
